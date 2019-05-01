c46e0dbf4fbcf38cd6f6c32faf0062d8decc3f22
Arthur Melin <arthur.melin@epitech.eu>
2019-05-01 22:20:01 +0200

    [Project] Initial setup

---
+++ src/main/java/fr/diabetips/api/DiabetipsApiApplication.java
@@ -0,0 +1,13 @@
+package fr.diabetips.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiabetipsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiabetipsApiApplication.class, args);
+    }
+
+}
