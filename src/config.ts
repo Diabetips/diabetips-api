27c9845aabfd5f9b5565260c630e36f93ed3314b
Arthur Melin <arthur.melin@epitech.eu>
2019-09-03 17:41:39 +0200

    [Refactor] Load config from JSON files

--- src/config.ts
+++ src/config.ts
@@ -6,66 +6,55 @@
 ** Created by Arthur MELIN on Wed Aug 28 2019
 */
 
-// tslint:disable-next-line:no-var-requires
-const pkg = require("../package.json");
+import { guard, loadJsonFile, merge } from "./lib/utils";
 
-const profiles = {
-    dev: {
-        db: {
-            type: "sqlite",
-            database: `${pkg.name}_dev.sqlite`,
-            synchronize: true,
-        },
-    },
-    test: {
-    },
-    preprod: {
-    },
-    prod: {
-    },
-};
+const env = process.env.DIABETIP_ENV || "dev";
 
-const profile = profiles.dev; // TODO: autoselect profile from env variable or w/e
+const baseConfig = loadJsonFile("config/config.json");
+const profileConfig = loadJsonFile(`config/config.${env}.json`);
+const pkg = guard(() => loadJsonFile("package.json"), {});
+
+const tmp = merge(baseConfig, profileConfig);
 
 export const config = {
-    db: {
-        ...profile.db,
-        entities: [
-            __dirname + "/entities/**/*.{js,ts}",
-        ],
-    },
-    http: {
-        host: "::",
-        port: "8080",
-    },
-    logger: {
-        appenders: {
-            console: {
-                type: "console",
-            },
-            file: {
-                type: "file",
-                filename: `logs/${pkg.name}.log`,
-                maxLogSize: 10 * 1024 * 1024,
-                backups: 10,
-                compress: true,
-                keepFileExt: true,
-            },
-        },
-        categories: {
-            default: {
-                appenders: ["console", "file"],
-                level: "all",
-            },
-            http: {
-                appenders: ["console", "file"],
-                level: "all",
-            },
-            sql: {
-                appenders: ["console", "file"],
-                level: "all",
-            },
-        },
-    },
+    ...tmp,
     pkg,
+    env,
 };
+
+function get(obj: any, path: string): any {
+    const sep = path.indexOf(".");
+    if (sep === -1) {
+        return obj[path];
+    } else {
+        return get(obj[path.slice(0, sep)], path.slice(sep + 1));
+    }
+}
+
+function resolve(obj: any) {
+    for (const key of Object.keys(obj)) {
+        if (typeof obj[key] === "object") {
+            resolve(obj[key]);
+        } else if (typeof obj[key] === "string") {
+            obj[key] = obj[key].replace(/[@%$]\{[^}]+\}/, (s: string) => guard(() => {
+                const m = s.match(/(?<type>.)\{(?<arg>.*)\}/);
+                if (m == null || m.groups == null) {
+                    return s;
+                }
+                const type = m.groups.type;
+                const arg = m.groups.arg;
+                switch (type) {
+                    case "@":
+                        return get(config, arg);
+                    case "$":
+                        return process.env[arg];
+                    case "%":
+                        // tslint:disable-next-line:no-eval
+                        return eval(arg);
+                }
+            }, s));
+        }
+    }
+}
+
+resolve(config);
---
+++ src/lib/utils.ts
@@ -0,0 +1,32 @@
+/*!
+** Copyright 2019 Diabetips
+**
+** All rights reserved
+**
+** Created by Arthur MELIN on Tue Sep 03 2019
+*/
+
+import fs = require("fs");
+
+export function guard<T>(fn: () => T, def: T): T {
+    try {
+        return fn();
+    } catch (err) {
+        return def;
+    }
+}
+
+export function loadJsonFile(path: string): any {
+    return JSON.parse(fs.readFileSync(path, { encoding: "utf-8" }));
+}
+
+export function merge(obj1: any, obj2: any): any {
+    const res = { ...obj1 };
+    for (const prop of Object.getOwnPropertyNames(obj2)) {
+        const value = obj2[prop];
+        obj1[prop] = (obj1[prop] !== undefined && typeof value === "object")
+            ? merge(obj1[prop], value)
+            : value;
+    }
+    return res;
+}
