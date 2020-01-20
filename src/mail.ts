1a1c82beedf27f96b6451495a827b7a654a637d2
Arthur Melin <arthur.melin@epitech.eu>
2020-01-20 01:09:35 +0000

    [Mail] Enable mail logging + verify connection at start

--- src/mail.ts
+++ src/mail.ts
@@ -11,8 +11,15 @@ import pug = require("pug");
 
 import { config } from "./config";
 import { getLang } from "./i18n";
+import { logger } from "./logger";
 
 const mail = nodemailer.createTransport(config.mail, config.mailDefaults);
+
+mail.verify((err, success) => {
+    if (err) {
+        logger.error(err.stack || err);
+    }
+});
 
 export async function sendMail(template: string,
                                lang: string,
