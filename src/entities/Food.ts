9591fcbf5312da878003ad5aebc96b3fb009f817
Arthur Melin <arthur.melin@epitech.eu>
2020-05-24 13:58:58 +0200

    Don't select datasrc in Food entity

--- src/entities/Food.ts
+++ src/entities/Food.ts
@@ -28,7 +28,7 @@ export class Food extends BaseEntity {
     @Column({ type: "float" })
     public sugars_100g: number;
 
-    @Column({ name: "datasrc", length: 100, nullable: true })
+    @Column({ name: "datasrc", length: 100, nullable: true, select: false })
     private _datasrc: string;
 
     @OneToOne((type) => FoodPicture, (pic) => pic.food)
