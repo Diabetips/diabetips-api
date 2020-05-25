998830cdc00485324422c5d9a9639cc4d6305f66
Arthur Melin <arthur.melin@epitech.eu>
2020-05-26 00:19:28 +0200

    Improve food search

--- src/entities/Food.ts
+++ src/entities/Food.ts
@@ -59,10 +59,9 @@ export class Food extends BaseEntity {
             qb = qb.andWhere("food.deleted = false");
         }
         if (req.name !== undefined && req.name !== "") {
-            const ranker = "ts_rank_cd(food.datalex, phraseto_tsquery('french', :name), 2) * sqrt(food.datarank)";
             qb = qb
-                .where(ranker + " > 0")
-                .orderBy(ranker, "DESC")
+                .where("ts_rank_cd(food.datalex, phraseto_tsquery('french', :name)) > 0")
+                .orderBy("ts_rank_cd(food.datalex, phraseto_tsquery('french', :name), 2) * sqrt(food.datarank)", "DESC")
                 .setParameter("name", req.name);
         } else {
             qb.orderBy("food.datarank", "DESC");
