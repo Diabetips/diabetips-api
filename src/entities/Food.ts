54894ee1f36b60e8e38ec61b71b00b70e6df0504
Anonymous <noreply@example.org>
2020-09-20 19:03:55 +0200

    I didn't want to do this

--- src/entities/Food.ts
+++ src/entities/Food.ts
@@ -19,7 +19,9 @@ export { FoodPicture };
 @Entity()
 export class Food extends BaseEntity {
 
-    // Indexed on prod server using GIN and gin_trgm_ops, but index types are not supported by TypeORM.
+    // Manually indexed with:
+    // CREATE INDEX "IDX_0f9580637d3bcdd0c9d6558de0" ON "food" USING GIN ("name" gin_trgm_ops);
+    // Note: gin_trgm_ops requires the "pg_trgm" extension
     @Column({ length: 300 })
     public name: string;
 
@@ -50,18 +52,20 @@ export class Food extends BaseEntity {
     @Column({ nullable: true })
     public nutriscore?: string;
 
-    // Information about food data source
-    // For OpenFoodFacts: "openfoodfacts:" + code
+    // Metadata about food data source
+    // OpenFoodFacts: "openfoodfacts:" + code
     @Column({ name: "datasrc", length: 100, nullable: true, select: false })
     private _datasrc: string;
 
-    // For OpenFoodFacts data: unique_scans_n
+    // Build with:
+    // OpenFoodFacts: unique_scans_n
     @Index()
     @Column({ name: "datarank", type: "bigint", default: 0, select: false })
     private _datarank: number;
 
-    // Indexed on prod server using GIN, but index types are not supported by TypeORM.
-    // Value = to_tsvector('french', name), not a default because can't reference another column in expr.
+    // Build with: UPDATE "food" SET "datarank" = to_tsvector('french', name);
+    // Manually indexed with:
+    // CREATE INDEX "IDX_1419635bc95b3829591127e448" ON "food" USING GIN ("datalex");
     @Column({ name: "datalex", type: "tsvector", nullable: true, select: false })
     private _datalex: number;
 
@@ -83,12 +87,13 @@ export class Food extends BaseEntity {
         if (req.name !== undefined && req.name !== "") {
             const query = req.name;
             qb = qb
-                .andWhere(new Brackets((qb) => {
+                .andWhere(new Brackets((bqb) => {
                     query.split(" ").forEach((keyword, index) => {
                         const param: { [key: string]: any } = {};
                         param[`keyword${index}`] = `%${keyword}%`;
-                        qb = qb.orWhere(`food.name ILIKE :keyword${index}`, param);
+                        bqb = bqb.orWhere(`food.name ILIKE :keyword${index}`, param);
                     });
+                    return bqb;
                 }))
                 .orderBy("ts_rank_cd(food.datalex, phraseto_tsquery('french', :name), 2) * sqrt(food.datarank)", "DESC")
                 .setParameter("name", req.name);
