7e9333a4af0cb63a5e645348d77e5085f7b8f0b1
Arthur MELIN <arthur.melin@epitech.eu>
2020-12-09 21:07:41 +0100

    [Notifications] Remove test route

--- src/controllers/NotificationController.ts
+++ src/controllers/NotificationController.ts
@@ -13,29 +13,14 @@ import { Authorized, Pageable, UserContext } from "../lib";
 import { NotificationFcmTokenRegisterReq } from "../requests";
 import { NotificationService } from "../services";
 
-// Temp imports for test route
-import { config } from "../config";
-import { AuthService, UserService } from "../services";
-
 @JsonController("/v1/notifications")
-export class UserNotificationController {
+export class NotificationController {
 
     @Get("/")
     @Authorized("notifications")
     public async getAllNotifications(@Ctx() ctx: UserContext, @QueryParams() p: Pageable, @Res() res: Response) {
         const page = await NotificationService.getAllNotifications(ctx.auth.uid, p);
         return page.send(res);
-    }
-
-    // DEBUG
-    @Get("/test")
-    public async test(@Ctx() ctx: UserContext) {
-        const user = await UserService.getUser(ctx.auth.uid);
-        const imageToken = await AuthService.generateUrlAccessToken(user);
-        await NotificationService.sendNotification(user, "test", {
-            foo: "aled",
-            bar: "oskour",
-        }, `${config.diabetips.apiUrl}/v1/users/me/picture?token=${imageToken}`);
     }
 
     @Post("/fcm_token")
